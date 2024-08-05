868b2c8d0fda107c4571468d9eea2e1014db7ff6
Samuel Bernsen <81886976+samuelbernsen08@users.noreply.github.com>
2024-08-05 14:43:48 -0700

    Hello World!

--- IntroAndBasics/Classwork0.py
+++ IntroAndBasics/Classwork0.py
@@ -2,12 +2,3 @@
 ### Uncomment the following line and run it
 
 # print("Hello World!")
-
-### With your groupmates, translate the following base 10 numbers into binary on paper:
-# - 17
-# - 16
-# - 256
-# - 400
-# - 2
-# - 1
-# - 8
