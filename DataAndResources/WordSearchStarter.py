b6e4d8f6352ddb82658440a17098b2cebd70673e
Samuel Bernsen <81886976+samuelbernsen08@users.noreply.github.com>
2024-12-14 13:33:10 -0800

    Add files via upload

---
+++ DataAndResources/WordSearchStarter.py
@@ -0,0 +1,8 @@
+box = ["BEEFK", "SRWRE", "AOOOE", "PDZWT", "EEBRN"] # each string is a row
+possibleWords = ["beef", "rod", "rode", "sap", "row", "fee"]
+
+# box = ["TEGGSXTTBV", "QVGEPPCSLB", "WTBLCOOMOC", "NIUAHGRASS", "MMGEIOAZSC", "RUSBCJILOP", "KDDEKBNRMN", "CWORMSPCOE", "TAUFLOWERS", "VCSFEZGYBT"] # each string is a row
+# possibleWords = ["blossom", "bugs", "flowers", "rain", "mud", "eggs", "nest","chick","worms","grass","moss","big","no","do",]
+
+# box = ["EWAJFELLFJFLWFSTKU", "FRMJUCDTMHFEEAJOUH", "CATAFRULLITMSOWZJU", "XITDJOXFTSHXPGOMIM", "XNNXREOOELXZDPRXKA", "WVIPQBUNNYJCYXMROW", "SXSDEGGSODBLGQPSFX", "SUPUDDLECHICKRKJRF", "PLYBSPPRILRIEIAPZU", "SLVZDTFLGHVWRUKSRH", "SBUTTERFLYOVONPZSQ", "CUMUBYOCZLGVSBFKMW", "BDTLIYSNFANSJPYJNH", "LNVILUWPVKQAAASIGD", "OXZPMLSDXIWZDSFIIB", "SLVBLJENQTWYGJTXIE", "SLTJVZEZVEDKWGBEEB", "OVECITDVOHIHJTTIMG", "MBDMHRSDLWISMHYAKE", "LADYBUGQUBPDGALHEK"] # each string is a row
+# possibleWords = ["bee", "blossom", "bud", "bunny", "butterfly", "chick", "eggs","flower","grass","kite","ladybug","lamb","lily","nest","puddle","rain","seeds","sprout","tulip","worm"]
